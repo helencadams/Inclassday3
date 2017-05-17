87afaa19f29ef7b314b3371af1c7131b54692d1e
helencadams <helencadams@me.com>
2017-05-17 16:21:43 -0400

    Homework

---
+++ main.js
@@ -0,0 +1,45 @@
+function addNumbers(numberA, numberB) {
+  return(numberA + numberB);
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo) // 4
+
+
+// Write a function called yell that logs out an uppercase version of a string.
+
+function yell(x) {
+	return x.toUpperCase();
+}
+var result = yell("danielle");
+console.log(result);
+
+
+// Write a function called yell10 that uses your yell function to log out an uppercase version of a string 10 times.
+
+function yell10(x) {
+	return x.toUpperCase();
+}
+var result = yell10("ruby");
+
+
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+
+
+// Write a function called longest that returns the longest of two input strings or arrays.
+
+var bikeBrands = ["Trek","Giant","Specialized"];
+var appleProducts = ["iPhone","iPad","iPod"];
+
+console.log(bikeBrands.length);
+console.log(appleProducts.length);
