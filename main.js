29df6086b3904599405f8452c0ec89b7bbd7b936
helencadams <helencadams@me.com>
2017-05-17 17:57:23 -0400

    Commit

--- main.js
+++ main.js
@@ -10,36 +10,47 @@ console.log(twoPlusTwo) // 4
 // Write a function called yell that logs out an uppercase version of a string.
 
 function yell(x) {
-	return x.toUpperCase();
+
+console.log(x.toUpperCase());
 }
 var result = yell("danielle");
-console.log(result);
+
 
 
 // Write a function called yell10 that uses your yell function to log out an uppercase version of a string 10 times.
 
 function yell10(x) {
-	return x.toUpperCase();
+  var yellWord = x.toUpperCase();
+
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
+  console.log(yellWord);
 }
 var result = yell10("ruby");
 
 
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-console.log(result);
-
 
 // Write a function called longest that returns the longest of two input strings or arrays.
+function longest(a,b) {
 
+}
 var bikeBrands = ["Trek","Giant","Specialized"];
-var appleProducts = ["iPhone","iPad","iPod"];
+var appleProducts = ["iPhone","iPad","iPod","iMac"];
+
+  var result = longest(bikeBrands,appleProducts)
+
+  if ( {
+
+  }
+
+
 
 console.log(bikeBrands.length);
 console.log(appleProducts.length);
